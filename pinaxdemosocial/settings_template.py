0c92c398954484a8de2572888cfcb906fc12d3d2
oppianmatt <matt@oppian.com>
2011-04-13 12:01:57 +0100

    fixing database settings

--- pinaxdemosocial/settings_template.py
+++ pinaxdemosocial/settings_template.py
@@ -4,12 +4,16 @@ Settings file to use in deployments. Is a template that will have it's values fi
 DEBUG = $DEBUG
 TEMPLATE_DEBUG = DEBUG
 
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '$DB_NAME'              # Or path to database file if using sqlite3.
-DATABASE_USER = '$DB_USER'             # Not used with sqlite3.
-DATABASE_PASSWORD = '$DB_PASS'         # Not used with sqlite3.
-DATABASE_HOST = '$DB_HOST'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    "default": {
+        "ENGINE": "postgresql_psycopg2",
+        "NAME": '$DB_NAME',
+        "USER": '$DB_USER',
+        "PASSWORD": '$DB_PASS',
+        "HOST": '$DB_HOST',
+        "PORT": "",
+    }
+}
 
 ADMINS = (
     ('Stephen Hartley', 'steve@oppian.com'),
